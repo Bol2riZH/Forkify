5a3c62af8eb3e27245eee5edecba13036ede017d
Matthieu Reungoat <reungoat.matthieu@gmail.com>
2022-08-24 12:19:20 +0200

    cleaned git

---
+++ src/js/controller.js
@@ -0,0 +1,49 @@
+import * as model from './model';
+import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+
+    recipeView.renderSpinner();
+
+    // 1) loading recipe
+    await model.loadRecipe(id);
+
+    // 2) rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    console.log(err);
+    recipeView.renderError();
+  }
+};
+
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+
+    // GET SEARCH QUERY
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // LOAD SEARCH RESULT
+    await model.loadSearchResults(query);
+
+    // RENDER RESULTS
+    resultsView.render(model.getSearchResultsPage());
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
+};
+init();
