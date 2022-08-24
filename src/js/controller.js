c4793cf7fd280baad723c23a57203c0719cf9cde
Matthieu Reungoat <reungoat.matthieu@gmail.com>
2022-08-24 14:16:24 +0200

    implementing pagination

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from './model';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -37,13 +38,25 @@ const controlSearchResults = async function () {
 
     // RENDER RESULTS
     resultsView.render(model.getSearchResultsPage());
+
+    // RENDER INITIAL PAGINATION BUTTONS
+    paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
   }
 };
 
+const controlPagination = function (goToPage) {
+  // render new result
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  // render new pagination buttons
+  paginationView.render(model.state.search);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 };
 init();
