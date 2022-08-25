7b537dd1350c441fa0102de426c087dc93161ba0
Matthieu Reungoat <reungoat.matthieu@gmail.com>
2022-08-25 22:24:04 +0200

    uploading new recipe 3/3

--- src/js/controller.js
+++ src/js/controller.js
@@ -102,6 +102,12 @@ const controlAddRecipe = async function (newRecipe) {
     // Success message
     addRecipeView.renderMessage();
 
+    // Render bookmark view
+    bookmarksView.render(model.state.bookmarks);
+
+    // Change id in the url
+    window.history.pushState(null, '', `#${model.state.recipe.id}`);
+
     // Close form window
     setTimeout(function () {
       addRecipeView.toggleWindow();
