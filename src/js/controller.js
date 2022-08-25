898c40ab4b698d2aa6f13097831b85ff7be34728
Matthieu Reungoat <reungoat.matthieu@gmail.com>
2022-08-25 17:20:18 +0200

    storing bookmarks with localStorage

--- src/js/controller.js
+++ src/js/controller.js
@@ -17,16 +17,18 @@ const controlRecipes = async function () {
 
     // 0) Update results view to mark selected search result
     resultsView.update(model.getSearchResultsPage());
+
+    // 1) Updating bookmarks view
     bookmarksView.update(model.state.bookmarks);
 
-    // 1) loading recipe
+    // 2) loading recipe
     await model.loadRecipe(id);
 
-    // 2) rendering recipe
+    // 3) rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
     recipeView.renderError();
-    console.log(err);
+    console.error(err);
   }
 };
 
@@ -79,7 +81,12 @@ const controlAddBookmark = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
+const controlBookmarks = function () {
+  bookmarksView.render(model.state.bookmarks);
+};
+
 const init = function () {
+  bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
   recipeView.addHandlerUpdateServings(controlServings);
   recipeView.addHandlerAddBookmark(controlAddBookmark);
